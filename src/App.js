38b8b64309c6f717688d5761a202ea5d59cdbe34
Saravanan M <sarvan.m20@gmail.com>
2024-06-06 13:05:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./styles/App.scss";
+
+function App() {
+  return <div className="App">My Movie App</div>;
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>My Movie App</div>
+  )
+}
+
+export default Navbar
