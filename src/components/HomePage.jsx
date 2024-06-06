b4017ff4cd00f268fc0eaf3292de8ab1a8d7f0d3
Saravanan M <sarvan.m20@gmail.com>
2024-06-06 15:40:03 +0530

    search functionality implemented

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,27 +6,47 @@ const HomePage = () => {
   const API_KEY = "693677a4";
   const [movieData, setMovieData] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const res = await axios.get(
-          `https://www.omdbapi.com/?s=movie&type=movie&page=${page}&apikey=${API_KEY}`
+          `https://www.omdbapi.com/?s=${
+            query?.length > 0 ? query : "movie"
+          }&type=movie&page=${page}&t=batman&apikey=${API_KEY}`
         );
         setMovieData(res?.data?.Search);
         // console.log(res?.data?.Search, "movies data");
+        const totalResults = res?.data?.totalResults;
+        setTotalPages(Math.ceil(totalResults / 10));
       } catch (error) {
         console.log(error, "Error while fetching movies");
       }
     };
 
     fetchMovies();
-  }, []);
+  }, [page, search]);
+
+  const handleClick = () => {
+    setSearch(!search);
+    if (search && query?.length > 0) {
+      setQuery("");
+    }
+  };
 
   return (
     <div className="homepage-container">
       <div className="search-container">
-        <input type="text" placeholder="Search.." />
+        <input
+          type="text"
+          placeholder="Movie name.."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <button onClick={handleClick}>{!search ? "Search" : "Clear"}</button>{" "}
       </div>
       <div className="movies-container">
         {movieData?.map((e, i) => {
@@ -40,6 +60,26 @@ const HomePage = () => {
             </div>
           );
         })}
+      </div>
+      <div className="pagination-caontainer">
+        <button
+          disabled={page === 1}
+          onClick={() => {
+            setPage(page > 1 ? page - 1 : 1);
+            console.log("working");
+          }}
+        >
+          Previous
+        </button>
+        <div>
+          Page {page} of {totalPages}
+        </div>
+        <button
+          onClick={() => setPage(page < totalPages ? page + 1 : totalPages)}
+          disabled={page === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/Navbar.scss";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <nav>
-      <h2>Movie App</h2>
+      <h2 onClick={handleClick}>Movie App</h2>
     </nav>
   );
 };
