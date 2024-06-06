a779e2fddda5542f8b0176430602258a67e5564c
Saravanan M <sarvan.m20@gmail.com>
2024-06-06 15:47:43 +0530

    skeleton added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "../styles/HomePage.scss";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HomePage = () => {
   const API_KEY = "693677a4";
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   const [movieData, setMovieData] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState();
@@ -13,6 +18,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
+        setIsLoading(true);
         const res = await axios.get(
           `https://www.omdbapi.com/?s=${
             query?.length > 0 ? query : "movie"
@@ -22,8 +28,10 @@ const HomePage = () => {
         // console.log(res?.data?.Search, "movies data");
         const totalResults = res?.data?.totalResults;
         setTotalPages(Math.ceil(totalResults / 10));
+        setIsLoading(false);
       } catch (error) {
         console.log(error, "Error while fetching movies");
+        setIsLoading(false);
       }
     };
 
@@ -49,17 +57,37 @@ const HomePage = () => {
         <button onClick={handleClick}>{!search ? "Search" : "Clear"}</button>{" "}
       </div>
       <div className="movies-container">
-        {movieData?.map((e, i) => {
-          return (
-            <div className="movie-card" key={e?.imdbID}>
-              <div className="movie-image">
-                <img src={e?.Poster} alt="" />
-              </div>
-              <h3>{e?.Title}</h3>
-              <div>Released year: {e?.Year}</div>
-            </div>
-          );
-        })}
+        {isLoading
+          ? Array(10)
+              .fill("")
+              .map((e, i) => {
+                return (
+                  <div className="movie-card" key={i}>
+                    <div className="movie-image">
+                      <Skeleton width={"100%"} height={"100%"} />
+                    </div>
+                    <Skeleton width={225} height={40} />
+                    <Skeleton width={200} height={30} />
+                  </div>
+                );
+              })
+          : movieData?.map((e, i) => {
+              return (
+                <div
+                  className="movie-card"
+                  key={e?.imdbID}
+                  onClick={() => {
+                    navigate(`/${e?.imdbID}`);
+                  }}
+                >
+                  <div className="movie-image">
+                    <img src={e?.Poster} alt="" />
+                  </div>
+                  <h3>{e?.Title}</h3>
+                  <div>Released year: {e?.Year}</div>
+                </div>
+              );
+            })}
       </div>
       <div className="pagination-caontainer">
         <button
